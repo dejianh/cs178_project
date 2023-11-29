9896bbff9f8890ebc38fb540fc5d9acf3a72662e
dejianh <dejianh@uci.edu>
2023-11-28 20:06:41 -0800

    added .gitignore

--- json_to_pandas.py
+++ json_to_pandas.py
@@ -4,7 +4,9 @@ import pandas as pd
 # Use your own file address
 data_file = open("yelp_dataset/yelp_academic_dataset_checkin.json") 
 data = []
+
 for line in data_file:
     data.append(json.loads(line))
+
 checkin_df = pd.DataFrame(data)
 data_file.close()
